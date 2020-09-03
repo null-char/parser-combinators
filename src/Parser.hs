26b498b4bbf802346b929f2238d9fe43d9664c26
SKP <skp.frl@gmail.com>
2020-09-04 01:42:40 +0530

    feat: add AST for parsing JSON

--- src/Parser.hs
+++ src/Parser.hs
@@ -1,24 +1,41 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Parser
   (
   )
 where
 
+import qualified Data.Map as M
+import qualified Data.Text as T
 import Text.Read (readMaybe)
 
-newtype Parser a = Parser {runParser :: String -> Maybe (a, String)}
+data JsonVal
+  = -- Maybe indicates the fractional part of the number. This will obviously be `Nothing` if it's
+    -- just a plain integer.
+    JsonNumber Integer (Maybe Integer)
+  | JsonBool Bool
+  | JsonNull
+  | JsonString T.Text
+  | JsonArray [JsonVal]
+  | JsonObj (M.Map T.Text JsonVal)
+  deriving (Eq, Show)
 
-parse :: Parser a -> String -> a
-parse m s =
-  case runParser m s of
-    Just (res, []) -> res
-    Just (_, rest) -> error "Parser did not consume entire stream"
+newtype Parser a = Parser {parse :: T.Text -> Maybe (a, T.Text)}
+
+runParser :: Parser a -> T.Text -> a
+runParser m s =
+  case parse m s of
+    Just (res, "") -> res
+    Just (_, rest) -> error "Parser did not consume the entire stream"
     Nothing -> error "Parser did not manage to parse anything"
 
-item = Parser $ \s ->
+-- A test parser
+digit :: Parser Int
+digit = Parser $ \s ->
   case s of
-    [] -> Nothing
-    (c : cs) ->
-      let rc = readMaybe [c] :: Maybe Int
-       in case rc of
+    "" -> Nothing
+    t ->
+      let ch = T.head t; cs = T.tail t
+       in case readMaybe [ch] of
             Just x -> Just (x, cs)
             Nothing -> Nothing
